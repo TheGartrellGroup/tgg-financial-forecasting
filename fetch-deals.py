df225c01dbd6ff0c459f8400f8d5b08cf172ea72
Jesse White <jesse.white6@gmail.com>
2024-03-08 15:02:49 -0600

    update query

--- fetch-deals.py
+++ fetch-deals.py
@@ -9,23 +9,28 @@ load_dotenv(find_dotenv())
 apiKey = os.environ.get("API_KEY")
 boardId = os.environ.get("DEALS_BOARD")
 apiUrl = "https://api.monday.com/v2"
-headers = {"Authorization": apiKey, "API-Version": "2023-04"}
+headers = {"Authorization": apiKey, "API-Version": "2023-10"}
 
 query = f"""
 {{
-  boards(ids: {boardId}) {{
-    items_page {{
-      items {{
-        id
-        name
-        column_values(ids: ["numbers", "numbers0", "numbers6", "text", "date", "date9"]) {{
-          value
-          text
-          column {{
-            id
-            title
-            description
-          }}
+  items_page_by_column_values(
+    board_id: {boardId}
+    columns: {{column_id: "status", column_values: ["RFP Submitted", "Lead", "Proposal", "Negotiation", "Contract Sent", "Won", "Default Label", "Scope/Discovery"]}}
+  ) {{
+    cursor
+    items {{
+      id
+      name
+      state
+      column_values(
+        ids: ["status", "numbers", "numbers0", "numbers6", "text", "date", "date9"]
+      ) {{
+        value
+        text
+        column {{
+          id
+          title
+          description
         }}
       }}
     }}
@@ -36,4 +41,6 @@ query = f"""
 data = {"query": query}
 
 response = requests.post(url=apiUrl, json=data, headers=headers)
-print(response.json())
+results = response.json()
+
+print(results)
