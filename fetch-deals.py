5d7dffa3a8c4ef05769270bd4c599ce004581482
Jesse White <jesse.white6@gmail.com>
2024-03-08 12:07:36 -0600

    add env and updated query

--- fetch-deals.py
+++ fetch-deals.py
@@ -1,24 +1,36 @@
 import requests
 import json
+import os
+import os.path
 
-apiKey = ''
+apiKey = os.environ.get("API_KEY")
+boardId = os.environ.get("DEALS_BOARD")
 apiUrl = "https://api.monday.com/v2"
-headers = {"Authorization" : apiKey, "API-Version" : "2023-04"}
+headers = {"Authorization": apiKey, "API-Version": "2023-04"}
 
-query = '''
-query {
-    boards(ids: 5045561244) {
-        items_page {
-            items {
-                id 
-                name 
-            }
+query = """
+{
+  boards(ids: {boardId}) {
+    items_page {
+      items {
+        id
+        name
+        column_values(ids: ["numbers", "numbers0", "numbers6", "text", "date", "date9"]) {
+          value
+          text
+          column {
+            id
+            title
+            description
+          }
         }
+      }
     }
+  }
 }
-'''
+"""
 
-data = {'query' : query}
+data = {"query": query}
 
 response = requests.post(url=apiUrl, json=data, headers=headers)
 print(response.json())
