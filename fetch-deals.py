6a2b1505e84f761943bbd6936196cf7ea6f6f7d4
Jesse White <jesse.white6@gmail.com>
2024-03-12 16:16:39 -0500

    add data beautification from Brian

--- fetch-deals.py
+++ fetch-deals.py
@@ -4,6 +4,10 @@ import os
 import os.path
 from dotenv import load_dotenv, find_dotenv
 
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 load_dotenv(find_dotenv())
 
 apiKey = os.environ.get("API_KEY")
@@ -43,4 +47,51 @@ data = {"query": query}
 response = requests.post(url=apiUrl, json=data, headers=headers)
 results = response.json()
 
+project_list = []
+for monday_project in results["data"]["items_page_by_column_values"]["items"]:
+    project_dict = {}
+    project_dict["project_name"] = monday_project["name"]
+    project_dict["project_id"] = monday_project["id"]
+    project_dict["project_state"] = monday_project["state"]
+
+    # Iterate over column values
+    for board_column in monday_project["column_values"]:
+        column_value = board_column["text"]
+        column_name = board_column["column"]["title"]
+        project_dict[column_name] = column_value
+
+    print(project_dict["project_name"])
+    project_list.append(project_dict)
+
+print("Done formatting Monday.com results....")
+
 print(results)
+
+
+def update_values(spreadsheet_id, range_name, value_input_option, _values):
+    creds, _ = google.auth.default()
+    try:
+        service = build("sheets", "v4", credentials=creds)
+        values = [
+            [
+                # Cell values ...
+            ],
+            # Additional rows ...
+        ]
+        body = {"values": values}
+        result = (
+            service.spreadsheets()
+            .values()
+            .update(
+                spreadsheetId=spreadsheet_id,
+                range=range_name,
+                valueInputOption=value_input_option,
+                body=body,
+            )
+            .execute()
+        )
+        print(f"{result.get('updatedCells')} cells updated.")
+        return result
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
